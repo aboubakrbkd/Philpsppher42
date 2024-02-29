24c06d3a55c8ae66072e30430d2a5e8e2de81a29
Aboubakr Boukdidi <aboukdid@e1r4p14.1337.ma>
2024-02-29 17:24:20 +0100

    jut some tests

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: aboukdid <aboukdid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:57:58 by aboukdid          #+#    #+#             */
-/*   Updated: 2024/02/28 09:03:04 by aboukdid         ###   ########.fr       */
+/*   Updated: 2024/02/29 17:15:35 by aboukdid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void	parse_argument(char **argv, int argc, t_list *philo)
+void	parse_argument_and_fill(char **argv, int argc, t_table *philo)
 {
 	int i = 0;
 	while (argv[i])
@@ -21,12 +21,74 @@ void	parse_argument(char **argv, int argc, t_list *philo)
 			print_error();
 		i++;
 	}
+	philo->philo_nbr = ft_atoi(argv[1]);
+	philo->time_to_die = ft_atoi(argv[2]);
+	philo->time_to_eat = ft_atoi(argv[3]);
+	philo->time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		philo->number_of_meals = ft_atoi(argv[5]);
+	else
+		philo->number_of_meals = 0;
+}
+
+
+void	safe_mutex_unlock(pthread_mutex_t *mutex, int flag)
+{
+	if (flag == 1)
+		pthread_mutex_lock(mutex);
+	else if (flag == 0)
+		pthread_mutex_unlock(mutex);
+	else if (flag == 2)
+		pthread_mutex_init(mutex, NULL);
+	else if (flag == 3)
+		pthread_mutex_destroy(mutex);
+}
+
+void	safe_thread(pthread_t *thread, void *(*foo)(void *), void	*data, int flag)
+{
+	if(flag == 0)
+		pthread_create(thread, NULL, foo, data);
+	else if (flag == 1)
+		pthread_join(*thread, NULL);
+	else if (flag == 2)
+		pthread_detach(*thread);
+}
+
+void	philo_init(t_table *philo)
+{
+	int i = 0;
+	t_philo *philos;
+	while (i < philo->philo_nbr)
+	{
+		philos = philo->philos + i;
+		philos->id = i + 1;
+		philos->full = 0;
+		philos->meals_counter = 0;
+		philos->
+		
+	}
+}
+void	data_init(t_table *philo)
+{
+	int i = 0;
+	philo->end_simulation = 0;
+	philo->philos = malloc(sizeof(t_table) * philo->philo_nbr);
+	if (!philo->philos)
+		return(NULL);
+	philo->forks = malloc(sizeof(t_table) * philo->philo_nbr);
+	if (!philo->forks)
+		return (NULL);
+	while (i < philo->philo_nbr)
+	{
+		safe_mutex_unlock(&philo->forks[i].fork, 2);
+		philo->forks[i].fork_id = i; // debug;	
+	}
+	phill_init(philo);
+	
 }
 int main(int argc, char **argv)
 {
-	t_list *philo = malloc(sizeof(t_list));
-	if (!philo)
-		return(0);
+	t_table philo;
 	if (argc != 5 && argc != 6)
 	{
 		printf("not enough\n");
@@ -34,14 +96,8 @@ int main(int argc, char **argv)
 	}
 	else 
 	{
-		parse_argument(argv + 1, argc, philo);
-		philo->num_philo = ft_atoi(argv[1]);
-		philo->time_to_die = ft_atoi(argv[2]);
-		philo->time_to_eat = ft_atoi(argv[3]);
-		philo->time_to_sleep = ft_atoi(argv[4]);
-		if (argv[5])
-			philo->number_of_meals = ft_atoi(argv[5]);
-		else
-			philo->number_of_meals = 0;
+		parse_argument_and_fill(argv + 1, argc, &philo);
+		data_init(&philo);
 	}
 }
+
--- philosopher.h
+++ philosopher.h
@@ -6,7 +6,7 @@
 /*   By: aboukdid <aboukdid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:58:13 by aboukdid          #+#    #+#             */
-/*   Updated: 2024/02/28 09:02:49 by aboukdid         ###   ########.fr       */
+/*   Updated: 2024/02/29 14:18:52 by aboukdid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,50 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <limits.h>
+#include <sys/time.h> 
 
-typedef struct s_list
+// typedef struct s_list
+// {
+//     long num_philo;
+//     long time_to_die;
+//     long time_to_eat;
+//     long time_to_sleep;
+//     long number_of_meals;
+//     struct s_list *next;
+// }			t_list;
+
+typedef struct s_fork
 {
-    long num_philo;
-    long time_to_die;
-    long time_to_eat;
-    long time_to_sleep;
-    long number_of_meals;
-    struct s_list *next;
-}			t_list;
+    pthread_mutex_t fork;
+    int fork_id;
+
+}               t_fork;
+
+typedef struct s_philo
+{
+    int id;//the id of the philo;
+    int meals_counter;//is the argv[5];
+    int full;//to check if he eats the max meals_counter or not;
+    int last_meal_time;//time past from last meal;
+    t_fork *left_fork;
+    t_fork *right_fork;
+    pthread_t thread_id;
+}               t_philo;
+typedef struct s_table
+{
+    int philo_nbr;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int number_of_meals;
+    int start_simulation;
+    int end_simulation;
+    t_fork *forks;
+    t_philo *philos;
+}               t_table;
+
+
+
 
 void	print_error();
 long	ft_atoi(char *str);
